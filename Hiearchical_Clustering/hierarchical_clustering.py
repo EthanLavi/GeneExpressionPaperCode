b8beed8e3df2c7d3f30734967881792aab5b422a
EthanLavi <89608076+EthanLavi@users.noreply.github.com>
2023-01-12 16:50:22 -0500

    Fixed issue with old code

--- Hiearchical_Clustering/hierarchical_clustering.py
+++ Hiearchical_Clustering/hierarchical_clustering.py
@@ -2,30 +2,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.cluster.hierarchy as shc
-from sklearn.cluster import AgglomerativeClustering
 import seaborn as sns
 
+# Clean data a bit
 data_scaled = pd.read_csv("SurvivalNormal.csv", sep=",")
-# Remove row numbers
 data_scaled = data_scaled.drop("Unnamed: 0", axis=1)
 data_scaled.head()
 
-print(data_scaled)
-print(data_scaled.columns)
-
-# Create Graphs
+# Dendogram
 plt.figure(figsize=(100, 7))
-plt.title("Dendrograms")
-dend = shc.dendrogram(shc.linkage(data_scaled, method='ward'))
+plt.title("Dendrogram: Gene Expression & Survival")
+matrix = shc.linkage(data_scaled.transpose(), method='ward', metric='euclidean')
+dend = shc.dendrogram(matrix, labels=data_scaled.columns)
 plt.show()
 
-cluster = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
-cluster.fit_predict(data_scaled)
-
-fig, ax = plt.subplots(figsize=(20, 10))
-sns.heatmap(data_scaled.corr(), center=0, annot=False, ax=ax)
-plt.show()
-
-sns.clustermap(data_scaled, metric='correlation', xticklabels=True)
+# Clustermap
+rowcolors = data_scaled["SURVIVAL"].map({0: "blue", 1: "red"})
+cmap = sns.clustermap(data_scaled, method='ward', metric='euclidean', row_colors=rowcolors, xticklabels=True,
+                      dendrogram_ratio=0.05)
+# Hide dendograms
+cmap.ax_col_dendrogram.set_visible(False)
+cmap.ax_row_dendrogram.set_visible(False)
+cmap.ax_cbar.set_visible(False)
+# Set text sizes on labels
+for label in cmap.ax_row_colors.get_xticklabels():
+    label.set(fontsize='x-small')
+for label in cmap.ax_heatmap.get_xticklabels():
+    label.set(fontsize='x-small')
+for label in cmap.ax_heatmap.get_yticklabels():
+    label.set(fontsize='xx-small')
+cmap.fig.suptitle('Heatmap: Gene Expression & Survival')
 plt.show()
 
